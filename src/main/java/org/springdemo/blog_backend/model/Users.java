7df08711c325af4cf03e42fabd049f301fcd2970
Seniyax <151843498+Seniyax@users.noreply.github.com>
2025-02-24 22:47:39 +0530

    added Users

---
+++ src/main/java/org/springdemo/blog_backend/model/Users.java
@@ -0,0 +1,27 @@
+package org.springdemo.blog_backend.model;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Users {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+
+    private String password;
+
+    private String email;
+
+    public Users() {}
+
+    public String getUsername() {
+        return username;
+    }
+}
